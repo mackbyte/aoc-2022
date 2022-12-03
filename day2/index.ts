958d49e7ff042ccb2eecdc11482ce5f5cb0e4798
Patrick Mackinder <paddymackinder@gmail.com>
2022-12-03 14:26:59 -0700

    Day 2 Part 1

---
+++ day2/index.ts
@@ -0,0 +1,1 @@
+export {default as part1} from "./part1"
---
+++ day2/part1.ts
@@ -0,0 +1,48 @@
+import {getInputLines} from "../common/inputUtils";
+
+enum Shape {
+    ROCK = 1,
+    PAPER = 2,
+    SCISSORS = 3,
+    UNKNOWN = 9
+}
+
+function getShape(code: string): Shape {
+    if (code === "A" || code === "X") {
+        return Shape.ROCK;
+    } else if (code === "B" || code === "Y") {
+        return Shape.PAPER;
+    } else if (code === "C" || code === "Z") {
+        return Shape.SCISSORS;
+    }
+
+    return Shape.UNKNOWN;
+}
+
+function calculateRoundScore(opponentShape: Shape, myShape: Shape) {
+    if (opponentShape === myShape) {
+        // draw
+        return 3 + myShape;
+    }
+
+    // win
+    if (opponentShape === Shape.ROCK && myShape === Shape.PAPER
+        || opponentShape === Shape.PAPER && myShape === Shape.SCISSORS
+        || opponentShape === Shape.SCISSORS && myShape === Shape.ROCK) {
+        return 6 + myShape;
+    }
+
+    // loss
+    return myShape;
+}
+
+export default function part1(): number {
+    const strategyLines = getInputLines(2);
+
+    return strategyLines
+        .map((strategy => {
+            const [opponent, me] = strategy.split(" ");
+            return calculateRoundScore(getShape(opponent), getShape(me));
+        }))
+        .reduce((total, current) => total + current, 0)
+}
