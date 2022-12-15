55fed648fa4cd8d36966d83fe26d007945861e3c
Patrick Mackinder <paddymackinder@gmail.com>
2022-12-15 12:31:44 -0700

    Day 8 Part 1

---
+++ day8/part1.ts
@@ -0,0 +1,76 @@
+import {getInputLines} from "../common/inputUtils";
+
+function printTrees(width: number, height: number, trees: Map<string, boolean>) {
+    const output: string[] = [];
+    for(let i = 0; i < height; i++) {
+        output.push('F'.repeat(width));
+    }
+
+    trees.forEach((value, coordinate) => {
+        let [x,y] = coordinate.split("-");
+        let row = parseInt(y);
+        let column = parseInt(x);
+        output[row] = output[row].substring(0, column) + "T" + output[row].substring(column + 1);
+    });
+
+    return output.join("\n");
+}
+
+export default function part1(): number | string {
+    const treeLines = getInputLines(8);
+    const trees: number[][] = [];
+    const visibleTreesCoordinates: Map<string, boolean> = new Map<string, boolean>();
+
+    treeLines.map(treeLine => {
+        let treeRow = treeLine.split("").map(tree => parseInt(tree));
+        trees.push(treeRow)
+    });
+
+    // scan top
+    trees[0].forEach((treeHeight, column) => {
+        let currentMax = treeHeight;
+        trees.forEach((treeRow, row) => {
+            if(row === 0 || treeRow[column] > currentMax) {
+                visibleTreesCoordinates.set(`${column}-${row}`, true);
+                currentMax = treeRow[column];
+            }
+        });
+    });
+
+    // scan left
+    trees.forEach((treeRow, row) => {
+        let currentMax = treeRow[0];
+        treeRow.forEach((treeHeight, column) => {
+            if(column === 0 || treeHeight > currentMax) {
+                visibleTreesCoordinates.set(`${column}-${row}`, true);
+                currentMax = treeHeight;
+            }
+        });
+    });
+
+    // scan right
+    trees.forEach((treeRow, row) => {
+        let currentMax = treeRow[treeRow.length-1];
+        treeRow.slice().reverse().forEach((treeHeight, column) => {
+            if(column === 0 || treeHeight > currentMax) {
+                visibleTreesCoordinates.set(`${treeRow.length-column-1}-${row}`, true);
+                currentMax = treeHeight;
+            }
+        });
+    });
+
+    // scan bottom
+    trees[trees.length-1].forEach((treeHeight, column) => {
+        let currentMax = treeHeight;
+        trees.slice().reverse().forEach((treeRow, row) => {
+            if(row === 0 || treeRow[column] > currentMax) {
+                visibleTreesCoordinates.set(`${column}-${trees.length-row-1}`, true);
+                currentMax = treeRow[column];
+            }
+        });
+    });
+
+    // console.log(printTrees(trees[0].length, trees.length, visibleTreesCoordinates));
+
+    return visibleTreesCoordinates.size;
+}
