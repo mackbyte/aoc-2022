b5b8cd14bfd0ce92f605b1d1db704d1852a01dd5
Patrick Mackinder <paddymackinder@gmail.com>
2022-12-16 10:58:34 -0700

    Refactor Day 9

--- day9/part1.ts
+++ day9/part1.ts
@@ -28,39 +28,64 @@ export function positionToString(pos: Position): string {
     return `${pos.x}-${pos.y}`
 }
 
-function updateTailPosition(tail: Position, head: Position): Position {
+export function updateTailPosition(tail: Position, head: Position): Position {
     let xDiff = Math.abs(tail.x-head.x);
     let yDiff = Math.abs(tail.y - head.y);
-    const dist = xDiff + yDiff;
+    const dist = xDiff + yDiff; // Manhattan distance
 
     if(dist < 2) {
+        // Definitely adjacent, don't move tail
         return tail;
     } else if (dist === 2) {
         if(tail.x === head.x) {
+            // 2 away on y axis, decrease distance to 1
             return {
                 x: tail.x,
                 y: tail.y > head.y ? tail.y - 1 : tail.y + 1
             }
         } else if(tail.y === head.y) {
+            // 2 away on x axis, decrease distance to 1
             return {
                 x: tail.x > head.x ? tail.x - 1 : tail.x + 1,
                 y: tail.y
             }
         } else {
+            // 1 away diagonally which counts as adjacent, don't move tail
             return tail;
         }
     } else {
-        if(xDiff === 1) {
+        // Greater than 2 total distance away, must move diagonally by changing x and y by 1
+        if(xDiff === 1 && yDiff === 2) {
+            // xDiff will now be 0, yDiff still 1 away
             return {
                 x: head.x,
                 y: tail.y + ((head.y-tail.y) * 0.5)
             }
-        } else {
+        } else if(yDiff === 1 && xDiff === 2) {
+            // yDiff will now be 0, xDiff still 1 away
             return {
                 x: tail.x + ((head.x-tail.x) * 0.5),
                 y: head.y
             }
+        } else {
+            // xDiff and yDiff still 1 away, positioned diagonally
+            return {
+                x: tail.x + ((head.x-tail.x) * 0.5),
+                y: tail.y + ((head.y-tail.y) * 0.5)
+            }
         }
+    }
+}
+
+export function move(pos: Position, dir: Direction): Position {
+    if(dir === Direction.UP) {
+        return {x: pos.x, y: pos.y+1}
+    } else if(dir === Direction.DOWN) {
+        return {x: pos.x, y: pos.y-1}
+    }else if(dir === Direction.LEFT) {
+        return {x: pos.x-1, y: pos.y}
+    } else {
+        return {x: pos.x+1, y: pos.y}
     }
 }
 
@@ -75,34 +100,11 @@ export default function part1(): number | string {
         const direction = getDirection(dirSymbol);
         const value = parseInt(val);
 
-        if(direction === Direction.UP) {
-            for(let i = 0; i < value; i++) {
-                head.y += 1;
-                tail = updateTailPosition(tail, head);
-                let positionKey = positionToString(tail);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
-        } else if (direction === Direction.DOWN) {
-            for(let i = 0; i < value; i++) {
-                head.y -= 1;
-                tail = updateTailPosition(tail, head);
-                let positionKey = positionToString(tail);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
-        } else if (direction === Direction.LEFT) {
-            for(let i = 0; i < value; i++) {
-                head.x -= 1;
-                tail = updateTailPosition(tail, head);
-                let positionKey = positionToString(tail);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
-        } else {
-            for(let i = 0; i < value; i++) {
-                head.x += 1;
-                tail = updateTailPosition(tail, head);
-                let positionKey = positionToString(tail);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
+        for(let i = 0; i < value; i++) {
+            head = move(head, direction);
+            tail = updateTailPosition(tail, head);
+            let positionKey = positionToString(tail);
+            positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
         }
     });
 
--- day9/part2.ts
+++ day9/part2.ts
@@ -1,46 +1,5 @@
 import {getInputLines} from "../common/inputUtils";
-import {Direction, getDirection, Position, positionToString} from "./part1";
-
-function updateTailPosition(tail: Position, head: Position): Position {
-    let xDiff = Math.abs(tail.x-head.x);
-    let yDiff = Math.abs(tail.y - head.y);
-    const dist = xDiff + yDiff;
-
-    if(dist < 2) {
-        return tail;
-    } else if (dist === 2) {
-        if(tail.x === head.x) {
-            return {
-                x: tail.x,
-                y: tail.y > head.y ? tail.y - 1 : tail.y + 1
-            }
-        } else if(tail.y === head.y) {
-            return {
-                x: tail.x > head.x ? tail.x - 1 : tail.x + 1,
-                y: tail.y
-            }
-        } else {
-            return tail;
-        }
-    } else {
-        if(xDiff === 1 && yDiff === 2) {
-            return {
-                x: head.x,
-                y: tail.y + ((head.y-tail.y) * 0.5)
-            }
-        } else if(yDiff === 1 && xDiff === 2) {
-            return {
-                x: tail.x + ((head.x-tail.x) * 0.5),
-                y: head.y
-            }
-        } else {
-            return {
-                x: tail.x + ((head.x-tail.x) * 0.5),
-                y: tail.y + ((head.y-tail.y) * 0.5)
-            }
-        }
-    }
-}
+import {getDirection, move, Position, positionToString, updateTailPosition} from "./part1";
 
 export default function part2(): number | string {
     const instructions = getInputLines(9);
@@ -52,42 +11,13 @@ export default function part2(): number | string {
         const direction = getDirection(dirSymbol);
         const value = parseInt(val);
 
-        if(direction === Direction.UP) {
-            for(let i = 0; i < value; i++) {
-                parts[0].y += 1;
-                for(let j = 1; j < parts.length; j++) {
-                    parts[j] = updateTailPosition(parts[j], parts[j-1]);
-                }
-                let positionKey = positionToString(parts[parts.length-1]);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
+        for(let i = 0; i < value; i++) {
+            parts[0] = move(parts[0], direction);
+            for(let j = 1; j < parts.length; j++) {
+                parts[j] = updateTailPosition(parts[j], parts[j-1]);
             }
-        } else if (direction === Direction.DOWN) {
-            for(let i = 0; i < value; i++) {
-                parts[0].y -= 1;
-                for(let j = 1; j < parts.length; j++) {
-                    parts[j] = updateTailPosition(parts[j], parts[j-1]);
-                }
-                let positionKey = positionToString(parts[parts.length-1]);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
-        } else if (direction === Direction.LEFT) {
-            for(let i = 0; i < value; i++) {
-                parts[0].x -= 1;
-                for(let j = 1; j < parts.length; j++) {
-                    parts[j] = updateTailPosition(parts[j], parts[j-1]);
-                }
-                let positionKey = positionToString(parts[parts.length-1]);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
-        } else {
-            for(let i = 0; i < value; i++) {
-                parts[0].x += 1;
-                for(let j = 1; j < parts.length; j++) {
-                    parts[j] = updateTailPosition(parts[j], parts[j-1]);
-                }
-                let positionKey = positionToString(parts[parts.length-1]);
-                positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
-            }
+            let positionKey = positionToString(parts[parts.length-1]);
+            positions.set(positionKey, (positions.get(positionKey) || 0) + 1);
         }
     });
 
